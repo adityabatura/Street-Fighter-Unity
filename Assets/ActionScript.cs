d4dabdfc535e3ba06f766593af263591beac78b7
kidshenlong <michaelpm91@googlemail.com>
2013-11-20 19:20:14 +0000

    Controls, Animation and Actions Scripts
    Controls, Animation and Actions are now handled by their own scripts.

---
+++ Assets/ActionScript.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionScript : MonoBehaviour {
+
+	public bool facingRight = true;
+	public float jumpForce = 400f;
+	public float moveForce = 20f;			// Amount of force added to move the player left and right.
+	public float maxSpeed = 2f;	
+
+	private bool jump = false;
+	private bool crouch = false;
+
+	private bool lp = false;
+	private bool mp = false;
+	private bool hp = false;
+	private bool lk = false;
+	private bool mk = false;
+	private bool hk = false;
+
+	private bool right = false;
+	private bool left = false;
+
+
+	private PlayerControlScript controller;
+	// Use this for initialization
+	void Start () {
+
+		controller = GetComponent <PlayerControlScript> ();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		jump = controller.jump;
+		crouch = controller.crouch;
+		lp = controller.lp;
+		mp = controller.mp;
+		hp = controller.hp;
+		lk = controller.lk;
+		mk = controller.mk;
+		hk = controller.hk;
+		left = controller.left;
+		right = controller.right;
+
+	}
+	void FixedUpdate () {
+		if(jump)
+		{
+			Jump();
+		}
+		if(crouch)
+		{
+			Crouch();
+		}
+
+		float h = Input.GetAxis("Horizontal");
+
+		if(right){
+			Right (h);
+		}
+		if(left){
+			Left (h);
+		}
+		/*if(h * rigidbody2D.velocity.x < maxSpeed && !crouch && grounded)
+		{
+
+		}*/
+
+		if(Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed)
+		{
+			rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+		}
+	}
+
+	void Jump(){
+		rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+		Debug.Log ("Jump");
+	}
+
+	void Crouch(){
+		Debug.Log ("Crouch");
+	}
+
+	void LP(){
+		Debug.Log ("Low Punch");
+	}
+	void MP(){
+		Debug.Log ("Mid Punch");
+	}
+	void HP(){
+		Debug.Log ("High Punch");
+	}
+	void LK(){
+		Debug.Log ("Low Kick");
+	}
+	void MK(){
+		Debug.Log ("Mid Kick");
+	}
+	void HK(){
+		Debug.Log ("High Kick");
+	}
+	void Right(float h){
+		rigidbody2D.AddForce(Vector2.right * h * moveForce);
+		Debug.Log ("Right");
+	}
+	void Left(float h){
+		rigidbody2D.AddForce(Vector2.right * h * moveForce);
+		Debug.Log ("Left");
+	}
+
+	void Flip ()
+	{
+		// Switch the way the player is labelled as facing.
+		facingRight = !facingRight;
+		
+		// Multiply the player's x local scale by -1.
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
--- Assets/PlayerControlScript.cs
+++ Assets/PlayerControlScript.cs
@@ -6,13 +6,12 @@ public class PlayerControlScript : MonoBehaviour {
 
 	private Transform groundCheck;			// A position marking where to check if the player is grounded.
 	public bool grounded = false;
+	public bool left = false;
+	public bool right = false;
 	public bool jump = false;
 	public bool crouch = false;
-	public bool facingRight = true;
-	public float jumpForce = 400f;
-	public float moveForce = 20f;			// Amount of force added to move the player left and right.
-	public float maxSpeed = 2f;	
-	// Use this for initialization
+
+
 	public bool lp = false;
 	public bool mp = false;
 	public bool hp = false;
@@ -20,6 +19,7 @@ public class PlayerControlScript : MonoBehaviour {
 	public bool mk = false;
 	public bool hk = false;
 
+	// Use this for initialization
 	void Start () {
 	
 	}
@@ -37,29 +37,53 @@ public class PlayerControlScript : MonoBehaviour {
 		// The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
 		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));  
 
-		if(Input.GetKeyDown(KeyCode.DownArrow) && grounded)
+		//Movement
+		if(Input.GetKeyDown(KeyCode.A) && grounded && !crouch)
+			left = true;
+		if(Input.GetKeyDown(KeyCode.D) && grounded && !crouch)
+			right = true;
+		if(Input.GetKeyUp(KeyCode.A) && grounded)
+			left = false;
+		if(Input.GetKeyUp(KeyCode.D) && grounded)
+			right = false;
+
+		if(Input.GetKeyDown(KeyCode.S) && grounded)
 			crouch = true;
-		if(Input.GetKeyUp(KeyCode.DownArrow) && grounded)
+		if(Input.GetKeyUp(KeyCode.S) && grounded)
 			crouch = false;
-		if(Input.GetButtonDown("Jump")||Input.GetKeyUp(KeyCode.UpArrow) && grounded)
+		if(Input.GetKeyDown(KeyCode.W) && grounded)
 			jump = true;
 		//Punches
-		if(Input.GetKeyDown(KeyCode.Q) && grounded)
+		if(Input.GetKeyDown(KeyCode.Y) && grounded)
 			lp = true;
-		if(Input.GetKeyDown(KeyCode.W) && grounded)
+		if(Input.GetKeyDown(KeyCode.U) && grounded)
 			mp = true;
-		if(Input.GetKeyDown(KeyCode.E) && grounded)
+		if(Input.GetKeyDown(KeyCode.I) && grounded)
 			hp = true;
 
+		if(Input.GetKeyUp(KeyCode.Y) && grounded)
+			lp = false;
+		if(Input.GetKeyUp(KeyCode.U) && grounded)
+			mp = false;
+		if(Input.GetKeyUp(KeyCode.I) && grounded)
+			hp = false;
+
 		//Kicks
-		if(Input.GetKeyDown(KeyCode.A) && grounded)	
+		if(Input.GetKeyDown(KeyCode.H) && grounded)	
 			lk = true;
-		if(Input.GetKeyDown(KeyCode.S) && grounded)
+		if(Input.GetKeyDown(KeyCode.J) && grounded)
 			mk = true;		
-		if(Input.GetKeyDown(KeyCode.D) && grounded)
+		if(Input.GetKeyDown(KeyCode.K) && grounded)
 			hk = true;	
-		if(Input.GetKeyDown(KeyCode.F))
-		{Flip (); Debug.Log("Flip");}
+
+		if(Input.GetKeyUp(KeyCode.H) && grounded)	
+			lk = false;
+		if(Input.GetKeyUp(KeyCode.J) && grounded)
+			mk = false;		
+		if(Input.GetKeyUp(KeyCode.K) && grounded)
+			hk = false;	
+		/*if(Input.GetKeyDown(KeyCode.F))
+		{Flip (); Debug.Log("Flip");}*/
 	}
 
 	void FixedUpdate(){
@@ -72,7 +96,7 @@ public class PlayerControlScript : MonoBehaviour {
 		//anim.SetFloat("speed", h);
 		
 		// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
-		if(h * rigidbody2D.velocity.x < maxSpeed && !crouch && grounded)
+		/*if(h * rigidbody2D.velocity.x < maxSpeed && !crouch && grounded)
 			// ... add a force to the player.
 			rigidbody2D.AddForce(Vector2.right * h * moveForce);
 		
@@ -80,15 +104,8 @@ public class PlayerControlScript : MonoBehaviour {
 		if(Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed)
 			// ... set the player's velocity to the maxSpeed in the x axis.
 			rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+*/
 
-		if(jump)
-		{
-			// Add a vertical force to the player.
-			rigidbody2D.AddForce(new Vector2(0f, jumpForce));
-			
-			// Make sure the player can't jump again until the jump conditions from Update are satisfied.
-			//jump = false;
-		}
 		/*if(jump && rigidbody2D.velocity.x>0)
 		{
 			rigidbody2D.AddForce(new Vector2(10f, jumpForce));
@@ -97,15 +114,6 @@ public class PlayerControlScript : MonoBehaviour {
 
 	}
 
-	void Flip ()
-	{
-		// Switch the way the player is labelled as facing.
-		facingRight = !facingRight;
-		
-		// Multiply the player's x local scale by -1.
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;
-	}
+
 
 }
